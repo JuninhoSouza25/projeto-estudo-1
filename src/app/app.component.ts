16a8fda6a349734c09087c11d075db7b18c5aae6
JuninhoSouza25 <dnt-bh@hotmail.com>
2025-02-08 11:10:12 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
+import { FeaturesSectionComponent } from "./components/features-section/features-section.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroSectionComponent, FeaturesSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
